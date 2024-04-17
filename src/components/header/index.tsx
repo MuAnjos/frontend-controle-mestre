e401f8437c3595d954369f9aeca7295ec82d1451
GerdanyJr <Asa_lindo@hotmail.com>
2024-04-17 15:49:39 -0300

    feat: :sparkles: add logo to module header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import { usePathname } from "next/navigation";
+import { LojaLogo } from "../../../public/svg/LojaLogo";
 
 export function Header() {
   const pathname = usePathname();
   const displayName = pathname.split("/")[2] ?? "Controle Mestre";
 
   return (
-    <header className="flex items-center justify-center py-4 bg-orange-500 rounded-b-2xl">
-      <h1 className="text-4xl font-bold text-white capitalize">
+    <header className="flex items-center justify-center py-6 bg-orange-500 rounded-b-2xl">
+      <div className="left-4 absolute">
+        {pathname.includes("module") && <LojaLogo />}
+      </div>
+      <h1 className="text-4xl mx-auto font-bold text-white capitalize">
         {displayName}
       </h1>
     </header>
