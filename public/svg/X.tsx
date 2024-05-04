039a689af42ec407c42542049c77464d7bb1cf1f
Gerdany Junior <Asa_lindo@hotmail.com>
2024-05-03 23:59:42 -0300

    feat: :sparkles: add icons

---
+++ public/svg/X.tsx
@@ -0,0 +1,24 @@
+export function X() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 26 26"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2 24L24 2"
+        stroke="white"
+        stroke-width="4"
+        stroke-linecap="round"
+      />
+      <path
+        d="M2 2L24 24"
+        stroke="white"
+        stroke-width="4"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+}
