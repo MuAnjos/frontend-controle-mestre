91e1d9882e09f01467d8ca44281b9f1510796769
GerdanyJr <asa_lindo@hotmail.com>
2024-07-14 19:42:52 -0300

    feat: :sparkles: create datepicker component

---
+++ src/components/UI/datepicker/index.tsx
@@ -0,0 +1,35 @@
+import "react-datepicker/dist/react-datepicker.css";
+import React, { forwardRef } from "react";
+import DatePicker, { DatePickerProps, registerLocale } from "react-datepicker";
+import { ptBR } from 'date-fns/locale/pt-BR';
+
+registerLocale("pt_BR", ptBR);
+
+interface CDatePickerProps extends React.HTMLAttributes<DatePickerProps> {
+    label: string;
+    invalid: boolean;
+    onChange: any;
+    selected: any;
+    register?: any;
+}
+
+export const CDatePicker = forwardRef(
+    (
+        { label, invalid, register, ...props }: CDatePickerProps, _
+    ) => {
+        return (
+            <div className="flex flex-col w-full p-2 bg-white rounded-lg">
+                <label htmlFor={props.id} className={`text-sm text-gray-500 ${invalid && "text-red-500"} false font-medium w-full`}>
+                    {label}
+                </label>
+                <DatePicker
+                    className="focus:outline-none"
+                    locale="pt_BR"
+                    maxDate={new Date()}
+                    {...register("dataDeNascimento")}
+                    {...props}
+                />
+            </div>
+        );
+    }
+);
